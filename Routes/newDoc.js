243a653823752530b19ecac22ce6693d770d3bb4
Naman <namanmail4@gmail.com>
2023-10-05 23:55:26 +0530

    cluttered

--- Routes/newDoc.js
+++ Routes/newDoc.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const router = express.Router();
-// const Admin = require("../models/CreateAdmin");
 const mongoose = require("mongoose");
 require("../db/conn");
-//new doc code
-//last updated till randomize allotting algorithm
+
 router.get("/", (req, res, next) => {
   res.status(200).json({
     message: "this is create message api get",
   });
 });
-// create new admin
+
 router.post("/", async (req, res) => {
   const date = new Date();
   collectionName = `trx${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
@@ -22,7 +20,6 @@ router.post("/", async (req, res) => {
   let Model;
   if (!collectionList.map((c) => c.name).includes(collectionName)) {
     const newCollectionSchema = new mongoose.Schema({
-      // define the schema as needed
       amount: String,
       transaction_id: String,
       username: String,
@@ -49,17 +46,14 @@ router.post("/", async (req, res) => {
 
   let sixdigitcode;
 
-  // Check if there are any used codes
   if (usedCodes.length > 0) {
     for (let i = 0; i < usedCodes.length; i++) {
-      // If there's a gap in the sequence, use it
       if (usedCodes[i + 1] - usedCodes[i] > 1) {
         sixdigitcode = String(usedCodes[i] + 1).padStart(6, "0");
         break;
       }
     }
 
-    // If no gap was found, use the next number
     if (!sixdigitcode) {
       sixdigitcode = String(usedCodes[usedCodes.length - 1] + 1).padStart(
         6,
@@ -67,7 +61,6 @@ router.post("/", async (req, res) => {
       );
     }
   } else {
-    // If no used codes, start from 000001
     sixdigitcode = "000001";
   }
   const register = new Model({
@@ -82,14 +75,6 @@ router.post("/", async (req, res) => {
   await register.save();
 
   res.send(collectionName);
-  //     const register = new newCollectionSchema({
-  //         amount: req.body.amount,
-  //         transaction_id: req.body.transaction_id,
-  //         username: req.body.username,
-  //         email_id: req.body.email_id,
-  //         currency: req.body.currency,
-  //       });
-  //    register.save();
 });
 
 module.exports = router;
